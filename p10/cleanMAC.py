a476792f0ba6b695dbdc7bba4cdab5f992485912
Akshay Bodla <akshaybodla21@gmail.com>
2022-12-07 13:58:05 -0600

    Added projects

---
+++ p10/cleanMAC.py
@@ -0,0 +1,13 @@
+import os
+def clean():
+    """
+    clean the cache in Mac OS
+    """
+    for root, dirs, files in os.walk('.'):
+        for item in dirs:
+            if (item[0]!='.'):
+                try:
+                    os.remove(os.path.join(item,'.DS_Store'))
+                    os.remove(os.path.join(item,'.ipynb_checkpoints'))
+                except:
+                    pass
